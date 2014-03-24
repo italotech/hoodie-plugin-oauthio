f8bdfdff8efabde180706117bf208086f08f8d2d
Jan Lehnardt <jan@apache.org>
2014-03-24 13:20:56 +0100

    add hooks templates

---
+++ hooks/static.js
@@ -0,0 +1,8 @@
+module.exports = {
+ 'server.pack.pre': function(args) {
+    // console.log('hook: server.pack.pre called');
+  },
+  'server.pack.post': function(args) {
+    // console.log('hook: server.pack.post called');
+  }
+};
