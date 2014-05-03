e69b338117151adca81ca0a96164699a0869e308
zoepage <o.gasidlo@gmail.com>
2014-05-03 22:17:08 +0200

    added pocket folder with structure and comments / examples of code

---
+++ pocket/assets/js/main.js
@@ -0,0 +1,8 @@
+/*
+	What would you like to do in the admin panel interface of the plugin aka pocket?
+	Do you want to show the admin some data? 
+	Or read the values of the input fields and store them?
+
+	Let the magic happen!
+	<3
+*/
