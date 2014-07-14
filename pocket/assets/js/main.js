4b0229cf4b1c8da6f1d1d50448435fb6d50d45a6
Alex Feyerke <kontakt@momoi.de>
2014-07-14 18:46:50 +0200

    enh: ready for pocket rename

--- pocket/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-/*
-	What would you like to do in the admin panel interface of the plugin aka pocket?
-	Do you want to show the admin some data? 
-	Or read the values of the input fields and store them?
-
-	Let the magic happen!
-	<3
-*/
