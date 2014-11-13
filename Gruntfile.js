519a0c097b0fe8082a02d1735fa58c03c935905e
Gabriel Mancini <gabriel.mancini@gmail.com>
2014-11-13 14:41:09 -0200

    test suite wip

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,90 +8,6 @@ module.exports = function (grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
-    useminPrepare: {
-      html: 'app/index.html',
-      options: {
-        dest: 'admin-dashboard'
-      }
-    },
-
-    usemin: {
-      html: ['admin-dashboard/index.html'],
-      css: ['admin-dashboard/styles/**/*.css'],
-      options: {
-        dirs: ['admin-dashboard']
-      }
-    },
-
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'app/images',
-          src: '**/*.{png,jpg,jpeg}',
-          dest: 'admin-dashboard/images'
-        }]
-      }
-    },
-
-    cssmin: {
-      dist: {
-        files: {
-          'admin-dashboard/styles/app.css': [
-            '.tmp/styles/**/*.css',
-            'app/styles/**/*.css'
-          ]
-        }
-      }
-    },
-
-    htmlmin: {
-      dist: {
-        options: {
-          /*removeCommentsFromCDATA: true,
-          // https://github.com/yeoman/grunt-usemin/issues/44
-          //collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeOptionalTags: true*/
-        },
-        files: [{
-          expand: true,
-          cwd: 'app',
-          src: '*.html',
-          dest: 'admin-dashboard'
-        }]
-      }
-    },
-
-    uglify: {
-      dist: {
-        files: {
-          'admin-dashboard/scripts/app.js': [
-            '.tmp/scripts/**/*.js'
-          ]
-        }
-      }
-    },
-
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: 'app',
-          dest: 'admin-dashboard',
-          src: [
-            '*.{ico,txt}',
-            '.htaccess'
-          ]
-        }]
-      }
-    },
-
     jshint: {
       files: [
         'Gruntfile.js',
@@ -154,57 +70,21 @@ module.exports = function (grunt) {
       }
     },
 
-    fakesmtp: {
-      test: {
-        options: {
-          dir: path.resolve(__dirname, 'test/browser/emails'),
-          port: 8888
-        }
-      }
-    },
-
     env: {
       test: {
         HOODIE_SETUP_PASSWORD: 'testing'
-      }
-    },
-
-    clean: {
-      dist: ['.tmp', 'admin-dashboard/*'],
-      server: '.tmp'
-    },
-
-
-    watch: {
-      livereload: {
-        files: [
-          'app/*.html',
-          '{.tmp,app}/styles/**/*.css',
-          '{.tmp,app}/scripts/**/*.js',
-          'app/images/**/*.{png,jpg,jpeg,webp}'
-        ],
-        tasks: ['livereload']
-      },
-      jshint: {
-        files: ['<%= jshint.files %>'],
-        tasks: 'jshint'
-      },
-      unittest: {
-        files: ['index.js', 'lib/*.js'],
-        tasks: 'test:unit'
       }
     }
 
   });
 
-  // custom tasks
+
   require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('test:unit', ['simplemocha:unit']);
   grunt.registerTask('test:browser', [
     'env:test',
     'shell:removeData',
-    'shell:removeEmails',
     'shell:npmLink',
     'shell:installPlugin',
     'hoodie',
@@ -235,25 +115,46 @@ module.exports = function (grunt) {
     'test:browser'
   ]);
 
-  grunt.registerTask('server', [
-    'clean:server',
-    'livereload-start',
-    'connect:livereload',
-    'open',
-    'watch'
+  grunt.registerTask('test:serve', [
+    'env:test',
+    'shell:removeData',
+    'shell:removeEmails',
+    'shell:npmLink',
+    'shell:installPlugin',
+    'hoodie',
+    'continueOn',
+  ]);
+  grunt.registerTask('test:stop', [
+    'hoodie_stop',
+    'shell:npmUnlink',
+    'shell:removePlugin'
   ]);
 
-  grunt.registerTask('build', [
-    'clean:dist',
-    'useminPrepare',
-    //'imagemin',
-    'htmlmin',
-    //'concat:generated',
-    //'cssmin:generated',
-    //'uglify:generated',
-    'concat',
-    'copy',
-    'usemin'
-  ]);
+  // grunt.registerTask('server', [
+  //   'clean:server',
+  //   'coffee:dist',
+  //   'handlebars',
+  //   'compass:server',
+  //   'livereload-start',
+  //   'connect:livereload',
+  //   'open',
+  //   'watch'
+  // ]);
+
+  // grunt.registerTask('build', [
+  //   'clean:dist',
+  //   'coffee:dist',
+  //   'handlebars',
+  //   'compass:dist',
+  //   'useminPrepare',
+  //   //'imagemin',
+  //   'htmlmin',
+  //   //'concat:generated',
+  //   //'cssmin:generated',
+  //   //'uglify:generated',
+  //   'concat',
+  //   'copy',
+  //   'usemin'
+  // ]);
 
 };
